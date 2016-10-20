e2ecdcdb28f30b6aba5351ad375ef16755d46151
yawkity <sehmon3@gmail.com>
2016-10-20 00:57:47 -0500

    Added OAuth access_token generation routes and implemented get_user functionality

--- index.js
+++ index.js
@@ -1,19 +1,51 @@
-var express = require('express');
 var http = require('http');
+var express = require('express');
+var api = require('instagram-node').instagram();
 var app = express();
 
 var urlString = "https://www.instagram.com/oauth/authorize/?client_id=73e5f31d13384082bc7f5b2cfc107c5d&redirect_uri=http://localhost:3000/auth&response_type=code"
+
+api.use({
+  client_id: "73e5f31d13384082bc7f5b2cfc107c5d",
+  client_secret: "1863b9b34a964298a3a0656b0af37ac7"
+});
+
+redirect_uri = "http://localhost:3000/auth"
+
+exports.authorize_user = function(req, res) {
+  res.redirect(api.get_authorization_url(redirect_uri, { scope: ['likes'], state: 'a state' }));
+};
+ 
+exports.handleauth = function(req, res) {
+  api.authorize_user(req.query.code, redirect_uri, function(err, result) {
+    if (err) {
+      console.log(err.body);
+      res.send("Didn't work");
+    } else {
+      console.log('Yay! Access token is ' + result.access_token);
+      api.use({access_token: result.access_token});
+      res.send('You made it!!');
+    }
+  });
+};
+
+// This is where you would initially send users to authorize 
+app.get('/authorize_user', exports.authorize_user);
+// This is your redirect URI 
+app.get('/auth', exports.handleauth);
 
 app.get('/', function(req, res) {
   res.send("Hello World");
 });
 
-app.get('/login', function(req, res){
-  res.redirect(urlString);
-});
-
-app.get('/auth', function(req, res){
-  res.send(req.query.code);
+app.get('/user', function(req, res) {
+  api.user('1823162936', function(err, result, remaining, limit){
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(result);
+    }
+  });
 });
 
 app.listen(3000, function(){
